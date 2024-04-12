0910ea762a0f279035afae053000c1cd238df59e
vicsurcor <vdsurcor@gmail.com>
2024-04-12 02:27:33 +0200

    Lista Zonas y Recycler View Item Base

---
+++ app/src/main/java/com/example/recuperacion_pmdm/ListaZonas.kt
@@ -0,0 +1,11 @@
+package com.example.recuperacion_pmdm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaZonas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_zonas)
+    }
+}
