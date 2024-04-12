da46ed7005ef0420ed6faaa65dd09400fefe74d4
vicsurcor <vdsurcor@gmail.com>
2024-04-12 06:09:51 +0200

    Navegacion Base

--- app/src/main/java/com/example/recuperacion_pmdm/MainActivity.kt
+++ app/src/main/java/com/example/recuperacion_pmdm/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.recuperacion_pmdm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var zonas: Button
+    private lateinit var favoritos: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        
+        findViewById(R.id.boton_Zonas)
+        findViewById(R.id.boton_Favoritos)
+
+        zonas.setOnClickListener {
+            intent = Intent(ListaZonas::)
+        }
     }
+
 }
