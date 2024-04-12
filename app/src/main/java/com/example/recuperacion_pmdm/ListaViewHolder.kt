a0d6f6fb3d5ff872c93d5eae0c9dfb60575f6856
vicsurcor <vdsurcor@gmail.com>
2024-04-12 05:58:58 +0200

    RecyclerView Base

---
+++ app/src/main/java/com/example/recuperacion_pmdm/ListaViewHolder.kt
@@ -0,0 +1,63 @@
+package com.example.recuperacion_pmdm
+
+import android.graphics.Color
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class ZonasAdapter (private val zonas: List<Zona>, tipo: String) : RecyclerView.Adapter<ZonasAdapter.ViewHolder>() {
+    val tipo = tipo
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val row: View = LayoutInflater.from(parent.context).inflate(R.layout.lista_item, parent, false)
+        return ViewHolder(row, tipo)
+    }
+
+    override fun getItemCount() = zonas.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val zona = zonas[position]
+        holder.bindRow(zona)
+    }
+    class ViewHolder(view: View, tipo: String) : RecyclerView.ViewHolder(view) {
+        val nombre: TextView = view.findViewById(R.id.item_Nombre)
+        val localidad: TextView = view.findViewById(R.id.item_Localidad)
+        val calidad: View = view.findViewById(R.id.item_Calidad)
+        val coordenadas: TextView = view.findViewById(R.id.item_Coordenadas)
+        val favoritos: ImageButton = view.findViewById(R.id.item_Favoritos)
+        val imagen: ImageView = view.findViewById(R.id.item_Imagen)
+        val tipo = tipo
+
+        fun bindRow(zona: Zona) {
+            nombre.text = zona.nombre
+            localidad.text = zona.localidad
+            if (zona.calidad == "Excelente"){
+                calidad.setBackgroundColor(Color.GREEN)
+            }
+            if (zona.calidad == "Buena"){
+                calidad.setBackgroundColor(Color.YELLOW)
+            }
+            if (zona.calidad == "Insuficiente") {
+                calidad.setBackgroundColor(Color.RED)
+            }
+            else {
+                calidad.setBackgroundColor(Color.GRAY)
+            }
+            coordenadas.text = zona.coordenadas
+            if (tipo == "lista") {
+                favoritos.setImageResource(R.drawable.ic_favoritos_no)
+            } else {
+                favoritos.setImageResource(R.drawable.ic_favoritos_si)
+            }
+            imagen.setImageResource(zona.imagen)
+
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/recuperacion_pmdm/Zonas.kt
@@ -0,0 +1,3 @@
+package com.example.recuperacion_pmdm
+
+class Zona(val nombre: String, val localidad: String, val calidad: String, val coordenadas: String, val imagen: Int)
